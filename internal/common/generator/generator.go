9c8164d79847f0cfc0d175d9c2f7fd8b5be91b43
goliatone <140582+goliatone@users.noreply.github.com>
2025-02-04 10:40:32 -0300

    refactor: functions that can be used in other places

--- internal/common/generator/generator.go
+++ internal/common/generator/generator.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/dave/jennifer/jen"
 )
@@ -60,4 +61,68 @@ func CreateOutputFile(dir string, f *jen.File) error {
 	}
 	defer out.Close()
 	return f.Render(out)
+}
+
+func FormatFuncType(fType *ast.FuncType) string {
+	var params, returns []string
+
+	if fType.Params != nil {
+		for _, param := range fType.Params.List {
+			paramType, _, _ := ExtractTypeInfo(param.Type)
+			if len(param.Names) > 0 {
+				for range param.Names {
+					params = append(params, paramType)
+				}
+			} else {
+				params = append(params, paramType)
+			}
+		}
+	}
+
+	if fType.Results != nil {
+		for _, result := range fType.Results.List {
+			returnType, _, _ := ExtractTypeInfo(result.Type)
+			if len(result.Names) > 0 {
+				for range result.Names {
+					returns = append(returns, returnType)
+				}
+			} else {
+				returns = append(returns, returnType)
+			}
+		}
+	}
+
+	funcStr := "func("
+	funcStr += strings.Join(params, ", ")
+	funcStr += ")"
+	if len(returns) > 0 {
+		if len(returns) == 1 {
+			funcStr += " " + returns[0]
+		} else {
+			funcStr += " (" + strings.Join(returns, ", ") + ")"
+		}
+	}
+
+	return funcStr
+}
+
+func ExtractTypeInfo(expr ast.Expr) (paramType, importPath, typePkg string) {
+	switch t := expr.(type) {
+	case *ast.StarExpr: // (*Type)
+		baseType, baseImport, basePkg := ExtractTypeInfo(t.X)
+		return "*" + baseType, baseImport, basePkg
+	case *ast.SelectorExpr: //  (pkg.Type)
+		if ident, ok := t.X.(*ast.Ident); ok {
+			typePkg = ident.Name
+			importPath = typePkg //NOTE: We assume last segment is the package name!
+			return typePkg + "." + t.Sel.Name, importPath, typePkg
+		}
+
+	case *ast.Ident: // (Type)
+		return t.Name, "", ""
+	case *ast.FuncType:
+		return FormatFuncType(t), "", ""
+	}
+
+	return "", "", ""
 }
